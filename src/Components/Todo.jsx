63f65862120da6d7392f26d3d279e4f47da383be
Jakub Światłoch <87209356+JakubSwiatlochgit@users.noreply.github.com>
2024-03-20 00:16:59 +0100

    BUGfix for edit input, styling improved, resposivity

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -2,10 +2,10 @@ import {useState} from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPenToSquare, faTrash } from "@fortawesome/free-solid-svg-icons";
 
-const Todo = ({ task, category, toggleComplete, removeTodo, handleEdit }) => {
+const Todo = ({ task, toggleComplete, removeTodo, handleEdit }) => {
 
   const [showEdit, setShowEdit] = useState(false)
-  const [newDesc, setNewDesc] = useState('');
+  const [newDesc, setNewDesc] = useState(task.desc);
 
   let checkLogic = `${task.completed ? "completed" : ""} ${
     task.isDaily ? "daily" : ""
@@ -23,41 +23,51 @@ const Todo = ({ task, category, toggleComplete, removeTodo, handleEdit }) => {
   return (
     <div className={containerClass}>
       <div className="row">
-        <div className="col-12 col-md-8 d-flex align-items-center">
+        <div className="col-12 col-md-6 text-center">
           <h3
             onClick={() => toggleComplete(task.id, task.completed)}
             className={`${checkLogic}`}
           >
             <p className="mb-0">Task:</p>
-            {task.desc}
+            
+            <p className="lead fw-bold">{task.desc}</p>
            
             <p className="mt-2 mb-0">Category:</p>
-            {task.category}
+            
+            <p className="lead fw-bold">{task.category}</p>
           </h3>
           
         </div>
         {
           showEdit && (
-            <div className="inputEdit">
-              <input 
-                type="text" 
-                value={newDesc} 
-                onChange={(e) => setNewDesc(e.target.value)} 
-              />
-              <button onClick={() => {
-                handleEdit(task.id, newDesc);
-                setShowEdit(false);
-              }}> Apply </button>
+            <div className="container col-12 col-md-6 d-flex align-items-center flex-column justify-content-center text-center">
+              <div className="row">
+                <div className="inputEdit">
+                  <input
+                    type="text"
+                    className="form-control"
+                    value={newDesc}
+                    onChange={(e) => setNewDesc(e.target.value)}
+                  />
+                  <button
+                  className="btn btn-success mt-3"
+                  onClick={() => {
+                    handleEdit(task.id, newDesc);
+                    setShowEdit(false);
+                  }}> Apply </button>
+                </div>
+              </div>
             </div>
           )
         }
-        <div className="col-12 col-md-4 d-flex justify-content-center align-items-center">
-          <div className="icons ">
+        <div className="col-12 mt-4 text-center d-flex justify-content-center align-items-center">
+          <div className="icons">
             <FontAwesomeIcon 
               icon={faPenToSquare} 
-              className="mx-4 icon icon-Edit"
+              className="icon icon-Edit"
               onClick={() => showEditFormHandler(task.id)} 
             />
+            <span className="mx-4"></span>
             <FontAwesomeIcon
               icon={faTrash}
               onClick={() => removeTodo(task.id)}
