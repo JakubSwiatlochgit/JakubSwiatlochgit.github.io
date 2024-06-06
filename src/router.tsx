08f9f917b69a92ee9357ea1fa5f702f1b6ed5654
Jakub Światłoch <87209356+JakubSwiatlochgit@users.noreply.github.com>
2024-06-06 10:18:38 +0200

    Update router.tsx

--- src/router.tsx
+++ src/router.tsx
@@ -5,9 +5,10 @@ import Contact from './Components/Contact.tsx'
 import App from './App.tsx'
 import TodoWrapper from './Components/TodoWrapper.tsx'
 
-
 export const router = createBrowserRouter([
   { path: "/", element: <App /> },
-  { path: "/dashboard", element:  <TodoWrapper/>},
+  { path: "/dashboard", element: <TodoWrapper /> },
   { path: "/contact", element: <Contact /> },
-])
+], {
+  basename: '/todoList-Iwai'
+})
